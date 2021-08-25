69ce458a6ff4750bf9a60250428e20cf117e4042
simindeymo <35273590+simindeymo@users.noreply.github.com>
2021-08-25 19:35:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from pyngrok import ngrok
+import socket
+import colorama
+import os
+
+ngrok_token = input('Ngrok token: ')
+username = input('NoIP Username: ')
+password = input('NoIP Password: ')
+host = input('NoIP Host: ')
+
+colorama.init(autoreset=True)
+
+localip = socket.gethostbyname(socket.gethostname())
+os.system('cls' if os.name=='nt' else 'clear')
+print('Local IP:')
+print(colorama.Fore.RED+localip)
+
+ngrok.set_auth_token(ngrok_token)
+ip = ngrok.connect().public_url[7:]
+
+os.system('cls' if os.name=='nt' else 'clear')
+print('Ngrok IP:')
+print(colorama.Fore.YELLOW+ip)
+
+r = requests.get(f"http://{username}:{password}@dynupdate.no-ip.com/nic/update?hostname={host}&myip={ip}")
+if r.status_code != requests.codes.ok:
+    print(r.content)
+
+os.system('cls' if os.name=='nt' else 'clear')
+print('Public IP:'[:i0])
+print(colorama.Fore.GREEN+host)
+
+while True:
+    pass
