9af37a4cdab40b333d51283d7f85feb1f648a238
simindeymo <35273590+simindeymo@users.noreply.github.com>
2021-08-26 12:50:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
-import requests
-from pyngrok import ngrok
-from noip_api import noip_api
-import socket
-import colorama
 import os
+try:
+    import requests
+    from pyngrok import ngrok
+    from noip_api import noip_api
+    import socket
+    import colorama
+except:
+    for i in ['pyngrok', 'noip-api-CodeGuy3',
+            'colorama', 'socket', 'requests']:
+        os.system('python -m pip install '+i)
+    os.system('python '+__file__)
 
 ngrok_token = input('Ngrok token: ')
+proto = input('Ngrok Protocol: ')
 username = input('NoIP Username: ')
 password = input('NoIP Password: ')
 host = input('NoIP Host: ')
 
 colorama.init(autoreset=True)
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+
 
 localip = socket.gethostbyname(socket.gethostname())
-os.system('cls' if os.name=='nt' else 'clear')
+
+clear()
 print('Local IP:')
 print(colorama.Fore.RED+localip)
 
-ngrok.set_auth_token(ngrok_token)
-ip = ngrok.connect().public_url[7:]
 
-os.system('cls' if os.name=='nt' else 'clear')
+ngrok.set_auth_token(ngrok_token)
+try:
+    ngrok.disconnect(get_tunnels()[0].public_url)
+except:
+    pass
+objip = ngrok.connect(25565, proto, bind_tls=True)
+ip = objip.public_url[8:]
+ipFromHost = socket.gethostbyname(ip)
+
+clear()
 print('Ngrok IP:')
-print(colorama.Fore.YELLOW+ip)
+print(colorama.Fore.YELLOW+ipFromHost)
+
 
 DDNS = noip_api.noip(host)
-DDNS.login(username=username, password=password)
-DDNS.setDNS(ip=ip)
+DDNS.login(username, password)
+DDNS.setDNS(ipFromHost)
 
-os.system('cls' if os.name=='nt' else 'clear')
+clear()
 print('Public IP:')
 print(colorama.Fore.GREEN+host)
+
 
 while True:
     pass
